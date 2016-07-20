3ce351222afd9baa87cbe9379ef2c71871e9fe67
Tim McMackin <timothymcmackin@yahoo.com>
2016-07-20 17:08:50 -0400

    Got to have the @Api on the resource for anything else to show up!

--- src/main/java/com/mkyong/rest/HelloWorldService.java
+++ src/main/java/com/mkyong/rest/HelloWorldService.java
@@ -1,21 +1,26 @@
 package com.mkyong.rest;
- 
+
+import io.swagger.annotations.*;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
- 
+
 @Path("/hello")
+@Api(value="/hello")
 public class HelloWorldService {
- 
+
 	@GET
 	@Path("/{param}")
+  //@Value("This command prints the message that you include in the {param} URL parameter.")
+  @ApiOperation(value="This command prints the message that you include in the {param} URL parameter.")
 	public Response getMsg(@PathParam("param") String msg) {
- 
+
 		String output = "Jersey say : " + msg;
- 
+
 		return Response.status(200).entity(output).build();
- 
+
 	}
- 
+
 }
