0195ce9039a4cfef07120e9056c762f12ada88db
Tim McMackin <timothymcmackin@yahoo.com>
2016-07-21 11:47:42 -0400

    simple POST that accepts JSON

--- src/main/java/com/mkyong/rest/HelloWorldService.java
+++ src/main/java/com/mkyong/rest/HelloWorldService.java
@@ -1,11 +1,19 @@
 package com.mkyong.rest;
 
 import io.swagger.annotations.*;
+import io.swagger.util.Json;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @Path("/hello")
 @Api(value="/hello")
@@ -18,10 +26,38 @@ public class HelloWorldService {
     		notes="It's a really simple command.")
 	public Response getMsg(@PathParam("param") String msg) {
 
-		String output = "Jersey say : " + msg;
+		String output = "Yor path parameter was : " + msg;
 
 		return Response.status(200).entity(output).build();
 
+	}
+	
+	@POST
+	@Path("/simplePost")
+	@Consumes({MediaType.APPLICATION_JSON})
+//	@Produces({MediaType.APPLICATION_JSON})
+	@ApiOperation(
+			value="Post a JSON file and get a response",
+			notes="Pass something interesting!"
+			)
+	@ApiResponses(
+			value = { 
+					@ApiResponse(code = 400, message = "Invalid input"),
+					@ApiResponse(code = 200, message = "It's all good")
+			})
+	public Response simplePost(
+//			@ApiParam(value="Input JSON", required=true)
+			String inputJsonString
+			){
+		String output;
+		try {
+			JSONObject inputJson = new JSONObject(inputJsonString);
+			output = "Hello! I got your file. The contents are: "+ inputJson.toString();
+		} catch (JSONException e) {
+			String errorMessage = "Bad JSON!";
+			return Response.status(400).entity(errorMessage).build();
+		}
+		return Response.status(200).entity(output).build();
 	}
 	
 	@GET
