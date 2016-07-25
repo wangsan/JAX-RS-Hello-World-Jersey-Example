6b3f57da7d2ba9292a7f7f8c3ac694a47d42b212
Tim McMackin <timothymcmackin@yahoo.com>
2016-07-25 16:34:16 -0400

    Message response in JSON

--- src/main/java/com/mkyong/rest/HelloWorldService.java
+++ src/main/java/com/mkyong/rest/HelloWorldService.java
@@ -56,7 +56,7 @@ public class HelloWorldService {
 	@ApiResponses(
 			value = {
 					@ApiResponse(code = 400, message = "Invalid input"),
-					@ApiResponse(code = 200, message = "It's all good", response=SimplePostSuccessResponse.class)
+					@ApiResponse(code = 200, message = "It's all good")
 			})
 	public Response simplePost(
 			@ApiParam(value="Input JSON", required=true)
